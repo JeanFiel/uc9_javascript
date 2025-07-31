cb2ad297494c717e7895c40901ca5d639b61b798
DasFiel <jeanhsantosfiel07@gmail.com>
2025-07-31 16:51:38 -0300

    Atividade variáveis
    Atividade sobre declaração e uso de variáveis simples.

---
+++ variaveis.js
@@ -0,0 +1,26 @@
+// variáveis são formas de guardar valores na memória.
+
+// Use o let para determinar uma variável. Ela pode ter qualquer valor.
+let idade = 15
+let texto = "Texto"
+
+// Concatenação: junção de variáveis e/ou textos 
+// Para concatenar, use +
+
+console.log("A minha idade é: "+ idade +"!\n")
+
+let nome = "Nome"
+console.log("Meu nome é "+nome+"\n")
+
+// Para somar, concatene números ou variáveis numéricas
+
+let idade_futura = idade + 5
+console.log("Daqui a 5 anos você terá: "+idade_futura+" anos.\n")
+
+//Atividade 1
+let aluguel = 1150
+let condominio = 250
+let energia = 95.9
+let totalmensal = aluguel + condominio + energia
+console.log("Meus gastos mensais são:\n" + "Aluguel: R$"+aluguel +"\nCondomínio: R$"+condominio+ "\nEnergria: R$"+energia+"\nTudo isso me custa: R$"+totalmensal+" por mês!")
+
