7a06060fbce82f78977b8c74e9b6afb2c45c94dc
DasFiel <jeanhsantosfiel07@gmail.com>
2025-08-01 17:08:13 -0300

    Atividade Café NF
    Atividade Não Finalizada sobre exibição de variáveis

---
+++ atividade_cafe.js
@@ -0,0 +1,29 @@
+let NomeUser1 = "Conrado"
+let NomeUser2 = "Pedrita"
+
+let IdadeUser1 = 15
+let IdadeUser2 = 19
+
+let User1Cliente = "False"
+let User2Cliente = "True"
+
+let CafesU1 = 187
+let CafesU2 = 5
+
+let Somacafes = CafesU1 + CafesU2
+
+let Qtdparasub = 200
+
+console.log("Olá\n"+"Bem-vindo ao sistema de usuário do Conradito\n"+"Abaixo estão as informações dos usuários cadastrados no sistema:\n")
+console.log("----Lista de usuários:")
+console.log("1 - Nome: "+NomeUser1+", idade: "+IdadeUser1+". É cliente: "+User1Cliente)
+console.log("2 - Nome: "+NomeUser2+", idade: "+IdadeUser2+". É cliente: "+User2Cliente)
+
+console.log("----Quantidade de cafés:")
+console.log("O Cliente "+NomeUser1+" ja tomou "+CafesU1+" cafés!")
+console.log("O Cliente "+NomeUser2+" ja tomou "+CafesU2+" cafés!")
+
+console.log("----Soma de cafés tomados:")
+console.log("Juntos, os clientes "+NomeUser1+ " e "+NomeUser2+ " tomaram "+(CafesU1+CafesU2)+" Cafés!")
+
+console.log("Quantos Cafés")
