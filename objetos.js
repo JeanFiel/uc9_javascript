611dbb112d9f9f98b029a89a5dcc2d22b2ab02db
DasFiel <jeanhsantosfiel07@gmail.com>
2025-08-06 17:05:39 -0300

    Objetos Simples
    Criação de Arrays de objetos simples

---
+++ objetos.js
@@ -0,0 +1,30 @@
+
+let usuarios = [
+{
+    id: 1,
+    nome: "Chaves",
+    idade: 8
+},
+{
+    id: 2,
+    nome: "Quico",
+    idade: 10
+},{
+    id: 3,
+    nome: "Seu Madruga",
+    idade: 44
+},{
+    id: 4,
+    nome: "Chiquinha",
+    idade: 8
+},
+{
+    id: 5,
+    nome: "Nhonho",
+    idade: 9
+},{
+    id: 6,
+    nome: "Bruxa do 71",
+    idade: 300
+}
+]
